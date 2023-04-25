9cb9d39033a357cb9b5c71b0e745a6fcdeb5d217
Компьютер <@DESKTOP-0RODP2R>
2023-04-25 22:12:33 +0200

    Добавьте файлы проекта.

---
+++ thirddottwo/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите длинну 1 катета: ");
+double a = double.Parse(Console.ReadLine());
+Console.Write("Введите длинну 2 катета: ");
+double b = double.Parse(Console.ReadLine());
+Console.Write("Введите длинну гипотенузы: ");
+double c = double.Parse(Console.ReadLine());
+
+if (Math.Pow(a, 2) + Math.Pow(b, 2) == Math.Pow(c, 2)) Console.WriteLine("Треугольник прямоугольный");
+else Console.WriteLine("Треугольник не прямоугольный");
